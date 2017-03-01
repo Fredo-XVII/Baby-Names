26f2618455cfd308c6894e5e7eed443f0600d181
Alfredo.G.Marquez <alfredo.g.marquez@gmail.com>
2017-02-28 23:18:39 -0600

    working through census tutorial

--- BabyNames_App/server.R
+++ BabyNames_App/server.R
@@ -1,21 +1,37 @@
 # server.R for BabyNames_App
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  #output$Name <- renderText({ 
-    #paste("You have selected this Name:")
-   # input$Name
-  
-  #output$TimeSeries <- renderPlot({
-    
-    #plot(National_Names$Year, National_Names$Count, data = subset(National_Names, Name = input$Name))
+library(maps)
+library(mapproj)
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
 
-  })
-})
+shinyServer(
+  function(input, output) {
+    
+    output$map <- renderPlot({
+      data <- switch(input$var, 
+                     "Percent White" = counties$white,
+                     "Percent Black" = counties$black,
+                     "Percent Hispanic" = counties$hispanic,
+                     "Percent Asian" = counties$asian)
+      color <- switch(input$var, 
+                      "Percent White" = "darkgreen",
+                      "Percent Black" = "black",
+                      "Percent Hispanic" = "darkorange",
+                      "Percent Asian" = "darkviolet")
+      
+      legend <- switch(input$var, 
+                       "Percent White" = "% White",
+                       "Percent Black" = "% Black",
+                       "Percent Hispanic" = "% Hispanic",
+                       "Percent Asian" = "% Asian")
+      
+      percent_map(var = data, 
+                  color = color, 
+                  legend.title = legend, 
+                  max = input$range[2], 
+                  min = input$range[1])
+    })
+      
+  }
+    )
--- BabyNames_App/ui.R
+++ BabyNames_App/ui.R
@@ -1,29 +1,25 @@
 # ui.R for BabyNames_App
 
-#
-shinyUI(
-  fluidPage(
-  
-  # Application title
-  titlePanel("Baby Names Popularity"),
+shinyUI(fluidPage(
+  titlePanel("censusVis"),
   
   sidebarLayout(
-    sidebarPanel( "sidebar panel",
+    sidebarPanel(
       helpText("Create demographic maps with 
-        information from the 2010 US Census.") #,
- 
-      #textInput("Name", 
-        #        label = "Type a Name to Analyze",
-       #         value = "John",
-      #verbatimTextOutput("value")
-      #) 
-    ) ,
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
     
-    mainPanel(textOutput("Name")
-      #plotOutput("TimeSeries") 
-      #textOutput("Name")
-    )
-  
-  ))
-)
+    mainPanel(plotOutput("map"))
+  )
+))
 
