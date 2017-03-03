9c3a177104bd140a2f84977fb64269dbb6edfb96
Alfredo.G.Marquez <alfredo.g.marquez@gmail.com>
2017-03-02 22:51:16 -0600

    Rendering the Input Name
    Was able to finally get the app to accept a name as an input.  Will push
    forward to see if I can use the input to select data from an R data set.

--- Baby_Names_App/app.R
+++ Baby_Names_App/app.R
@@ -18,32 +18,27 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        textInput(inputId = "Names" ,
+                  label = "Please enter a Name:",
+                  value = "John"
+                  )
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         textOutput("textDisplay")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+  
+  output$textDisplay <- renderText(
+    paste0("The name you chose:" , input$Names)
+  )
+    
+   }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
